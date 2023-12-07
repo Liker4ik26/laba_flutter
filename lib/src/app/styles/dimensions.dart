263407083e5ffcbc2fb42311244fae7fdb81a8a6
liker4ik26 <likakan85@gmail.com>
2023-12-08 02:37:04 +0600

    [Feat] Added styles and colors

--- lib/src/app/styles/dimensions.dart
+++ lib/src/app/styles/dimensions.dart
@@ -2,6 +2,8 @@ class AppDimensions {
   AppDimensions._();
 
   static const double extraSmall = 12;
-  static const double small = 16;
+  static const double small = 14;
+  static const double medium = 16;
   static const double tinyMedium = 38;
+  static const double circle = 100;
 }
